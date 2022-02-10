7698e0a06c34e3f2e7eeff3bf7fecf912ec87ca6
Diya-James <diyaperumthottam@gmail.com>
2022-02-10 14:32:50 +0530

    Form Insert

--- frontend/src/Insert.js
+++ frontend/src/Insert.js
@@ -5,23 +5,28 @@ function Insert() {
   return (
       <>
       <Header/>
-        <div>Insert</div>
-        <div>Insert</div>
-        <div>Insert</div>
-        <div>Insert</div>
-        <div>Insert</div>
-        <div>Insert</div>
-        <div>Insert</div>
-        <div>Insert</div>
-        <div>Insert</div>
-        <div>Insert</div>
-        <div>Insert</div>
-        <div>Insert</div>
-        <div>Insert</div>
-        <div>Insert</div>
-        <div>Insert</div>
-        <div>Insert</div>
-        <div>Insert</div>
+      <form>
+        <h1>Insert a Book</h1>
+        <div className="field">
+          <label>Title</label>
+          <input type="text" name="title" placeholder="Title" />
+        </div>
+        <div className="field">
+          <label>Author</label>
+          <input type="text" name="author" placeholder="Author" />
+        </div>
+        <div className="field">
+          <label>Publish Year</label>
+          <input type="text" name="year" placeholder="Year" />
+        </div>
+        <div className="field">
+          <label>Description</label>
+          <input type="text" name="description" placeholder="Description" />
+        </div>
+        <button classname="btn" type="submit">
+          Submit
+        </button>
+      </form>
       </>
   )
 }
