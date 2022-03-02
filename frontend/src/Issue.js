7c8d7aaa0066ab600abe4bceb950a317ff55956d
Abhinav-22 <abhinavanil9@gmail.com>
2022-03-02 23:30:14 +0530

    bug fixes

--- frontend/src/Issue.js
+++ frontend/src/Issue.js
@@ -4,27 +4,27 @@ import Axios from "axios";
 import "./Issue.css";
 import { useLocation } from "react-router-dom";
 
-
 function refreshPage() {
   window.location.reload(false);
 }
 function Issue() {
-
-  const {state} = useLocation();
-  const { btitle} = state;
+  const { state } = useLocation();
+  const { btitle } = state;
   const { bid } = state;
 
   var today = new Date();
-  var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-  var rdate = today.getFullYear() + '-' + (today.getMonth() + 2) + '-' + today.getDate();
-  console.log(date)
+  var date =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  var rdate =
+    today.getFullYear() + "-" + (today.getMonth() + 2) + "-" + today.getDate();
+  console.log(date);
   const [cid, setcid] = useState("");
   const [cname, setcname] = useState("");
   const [title, settitle] = useState("");
   const [bbid, setbid] = useState("");
   const [ddate, setdate] = useState("");
-   const [retdate, setretdate] = useState("");
-   
+  const [retdate, setretdate] = useState("");
+
   // setdate(date);
   // setretdate(rdate);
   //setbid(bid);
@@ -36,13 +36,18 @@ function Issue() {
       bname: btitle,
       bid: bid,
       date: ddate,
-      retdate: retdate
-    }).then(() => {
-      alert("successfull insert");
-      window.location.reload();
-      window.location.href = '/return'
-    });
-    
+      retdate: retdate,
+    })
+      .then((res) => {
+        window.location.reload();
+        window.location.href = "/return";
+      })
+      .catch((err) => {
+        alert(err);
+        window.location.reload();
+        window.location.href = "/return";
+      });
+
     // console.log(cid)
     // console.log(cname)
     // console.log(btitle)
@@ -73,89 +78,87 @@ function Issue() {
               </label>
             </div>
 
-          <div className="form-groupi">
-            <div>
-              <input
-                type="text"
-                name="cname"
-                id="cname"
-                className="form__inputi"
-                placeholder=" "
+            <div className="form-groupi">
+              <div>
+                <input
+                  type="text"
+                  name="cname"
+                  id="cname"
+                  className="form__inputi"
+                  placeholder=" "
                   autocomplete="off"
                   onChange={(e) => {
                     setcname(e.target.value);
                   }}
-              />
-              <label for="cname" className="form__labeli">
-                Customer Name
-              </label>
-            </div>
-            <div>
-              <input
-                type="text"
-                name="title"
-                id="title"
-                className="form__inputi"
-                placeholder=" "
-                autocomplete="off"
-                value = {btitle}
-                onChange={(e) => {
-                  settitle(btitle);
-                }}
+                />
+                <label for="cname" className="form__labeli">
+                  Customer Name
+                </label>
+              </div>
+              <div>
+                <input
+                  type="text"
+                  name="title"
+                  id="title"
+                  className="form__inputi"
+                  placeholder=" "
+                  autocomplete="off"
+                  value={btitle}
+                  onChange={(e) => {
+                    settitle(btitle);
+                  }}
                 />
                 <label for="title" className="form__labeli">
-                Book Name
-              </label>
+                  Book Name
+                </label>
                 <input
-                name="bbid"
-                id="bbid"
-                className="form__inputi"
-                placeholder=" "
-                autocomplete="off"
-                value = {bid}
-                onChange={(e) => {
-                  setbid(bid);
-                }}
+                  name="bbid"
+                  id="bbid"
+                  className="form__inputi"
+                  placeholder=" "
+                  autocomplete="off"
+                  value={bid}
+                  onChange={(e) => {
+                    setbid(bid);
+                  }}
                 />
                 <label for="bbid" className="form__labeli">
-                Book id
+                  Book id
                 </label>
-                <br/>
+                <br />
                 <input
                   type="date"
-                name="ddate"
-                id="ddate"
-                className="form__inputi"
-                placeholder=" "
-                autocomplete="off"
-                //value = {date}
-                onChange={(e) => {
-                  setdate(e.target.value);
-                }}
+                  name="ddate"
+                  id="ddate"
+                  className="form__inputi"
+                  placeholder=" "
+                  autocomplete="off"
+                  //value = {date}
+                  onChange={(e) => {
+                    setdate(e.target.value);
+                  }}
                 />
                 <label for="ddate" className="form__labeli">
-                Date of issueing
-              </label>
+                  Date of issueing
+                </label>
                 <input
-                type="date"
-                name="retdate"
-                id="retdate"
-                className="form__inputi"
-                placeholder=" "
-                autocomplete="off"
-                
-                //value = {rdate}
-                onChange={(e) => {
-                  setretdate(e.target.value);
-                }}
+                  type="date"
+                  name="retdate"
+                  id="retdate"
+                  className="form__inputi"
+                  placeholder=" "
+                  autocomplete="off"
+                  //value = {rdate}
+                  onChange={(e) => {
+                    setretdate(e.target.value);
+                  }}
                 />
                 <label for="retdate" className="form__labeli">
-                Date of return
-              </label>
-            </div>
-                
-              <br />
+                  Date of return
+                </label>
+              </div>
 
+              <br />
             </div>
             <button className="center" onClick={submitReview}>
               Submit
--- frontend/src/insert.js
+++ frontend/src/insert.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import Header from "./Header";
 import Axios from "axios";
 import "./Insert.css";
 
 function Insert() {
-
   const [id, setid] = useState("");
   const [title, settitle] = useState("");
   const [author, setauthor] = useState("");
@@ -19,32 +17,17 @@ function Insert() {
       author: author,
       category: category,
       description: description,
-    }).then(() => {
-      alert("successfull insert");
-    });
-    window.location.reload();
-      //window.location.href = ''
+    })
+      .then((res) => {
+        window.location.reload();
+        window.location.href = "/";
+      })
+      .catch((err) => {
+        alert(err);
+        window.location.reload();
+        window.location.href = "/";
+      });
   };
-
-  // const validate = (values) => {
-  //     const errors = {};
-  //     if (!values.title) {
-  //         errors.title = "Title is required";
-  //     }
-  //     if (!values.author) {
-  //         errors.author = "Author is required";
-  //     }
-  //     if (!values.description) {
-  //         errors.description = "Description is required";
-  //     }
-  //     if (!values.id) {
-  //         errors.id = "Id is required";
-  //     }
-  //     if (!values.category) {
-  //         errors.category = "Category is required";
-  //     }
-  //     return errors;
-  // };
 
   return (
     <div className="bbody">
